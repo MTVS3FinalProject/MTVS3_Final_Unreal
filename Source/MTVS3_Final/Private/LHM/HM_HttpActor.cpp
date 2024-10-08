983ebb2230e1779a2e513adce0400f4967e98e0d
lee <hyeonmin1012@gmail.com>
2024-10-08 20:20:39 +0900

    [Feat] LHM_HttpActor
    class 생성 및 변수, 함수 세팅

---
+++ Source/MTVS3_Final/Private/LHM/HM_HttpActor.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LHM/HM_HttpActor.h"
+
+// Sets default values
+AHM_HttpActor::AHM_HttpActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AHM_HttpActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AHM_HttpActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/MTVS3_Final/Public/LHM/HM_HttpActor.h
@@ -0,0 +1,69 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "HttpFwd.h"
+#include "HM_HttpActor.generated.h"
+
+UCLASS()
+class MTVS3_FINAL_API AHM_HttpActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AHM_HttpActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	/*#pragma region UI
+	UPROPERTY(EditAnywhere, Category = "Defalut|UI")
+	TSubclassOf<class UStartWidget> StartUIFactory;
+	UPROPERTY()
+	class UStartWidget* StartUI;
+#pragma endregion*/
+
+	void ReqPostVerifySignin(long UserId);
+
+	void OnResPostVerifySignin(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful);
+
+	void ReqPostOnVerifySignin(long UserId);
+
+	void OnResPostOnVerifySignin(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful);
+
+	void ReqPostSignin(bool bIsHost, FText Email, FText Password, FText Age, FString Gender, FText Nickname);
+
+	void OnResPostSignin(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful);
+
+	void ReqPostLogin(FText Email, FText Password);
+
+	void OnResPostLogin(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful);
+
+	void ReqPostJoinTTSession(long UserId, int64 TTSessionId);
+
+	void OnResPostJoinTTSession(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful);
+
+	void ReqPostApplyForSeat(long UserId, int64 SeatId);
+
+	void OnResPostApplyForSeat(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful);
+
+	void ReqPostVerifyBooking(long UserId, int64 SeatId);
+
+	void OnResPostVerifyBooking(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful);
+
+	void ReqPostOnVerifyBooking(long UserId, int64 SeatId);
+
+	void OnResPostOnVerifyBooking(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful);
+
+	void ReqPostCompleteBooking(long UserId, int64 SeatId);
+
+	void OnResPostCompleteBooking(FHttpRequestPtr Request , FHttpResponsePtr Response , bool bWasSuccessful);
+};
