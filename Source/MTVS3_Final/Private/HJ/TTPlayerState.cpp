6de5f6b2fe5e67e4c575d1a507987b6aee6ea6da
g0r0kke <jiniandjuni@gmail.com>
2024-10-10 10:09:00 +0900

    Feat: PlayerState 정보 설정

--- Source/MTVS3_Final/Private/HJ/TTPlayerState.cpp
+++ Source/MTVS3_Final/Private/HJ/TTPlayerState.cpp
@@ -1,5 +1,61 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "HJ/TTPlayerState.h"
 
+void ATTPlayerState::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Coin = InitialCoin;
+}
+
+void ATTPlayerState::SetbIsHost(const bool& _bIsHost)
+{
+	bIsHost = _bIsHost;
+}
+
+long ATTPlayerState::GetbIsHost() const
+{
+	return bIsHost;
+}
+
+void ATTPlayerState::SetNickname(const FString& _Nickname)
+{
+	Nickname = _Nickname;
+}
+
+FString ATTPlayerState::GetNickname() const
+{
+	if ( Nickname.IsEmpty() )
+	{
+		UE_LOG(LogTemp , Warning , TEXT("Nickname is empty , returning default value."));
+		return TEXT("Empty_Nickname");
+	}
+	return Nickname;
+}
+
+void ATTPlayerState::SetUserId(const int32& _UserId)
+{
+	UserId = _UserId;
+}
+
+long ATTPlayerState::GetUserId() const
+{
+	if ( !UserId )
+	{
+		UE_LOG(LogTemp , Warning , TEXT("UserId is empty , returning default value."));
+		return -1;
+	}
+	return UserId;
+}
+
+void ATTPlayerState::AddCoin(int32 _Coin)
+{
+	Coin += _Coin;
+}
+
+int32 ATTPlayerState::GetCoin()
+{
+	return Coin;
+}
