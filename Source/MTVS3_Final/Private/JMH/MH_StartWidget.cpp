daa30139f950427f4e3a14f50da5c40f4c03450d
may-04 <146904012+may-04@users.noreply.github.com>
2024-10-10 16:24:12 +0900

    MH : Start UI 작업중, Main UI 작업 시작 -> 미니맵, 시간 표시 먼저 시작할 예정

--- Source/MTVS3_Final/Private/JMH/MH_StartWidget.cpp
+++ Source/MTVS3_Final/Private/JMH/MH_StartWidget.cpp
@@ -4,32 +4,48 @@
 #include "JMH/MH_StartWidget.h"
 
 #include "Components/Button.h"
+#include "Components/WidgetSwitcher.h"
 
 void UMH_StartWidget::NativeConstruct()
 {
 	Super::NativeConstruct();
-
+	
 	Btn_Back->OnClicked.AddDynamic(this,&UMH_StartWidget::OnClickedBackButton);
 	Btn_Confirm->OnClicked.AddDynamic(this,&UMH_StartWidget::OnClickedConfirmButton);
 	Btn_Exit->OnClicked.AddDynamic(this,&UMH_StartWidget::OnClickedExitButton);
 	Btn_AddPicture->OnClicked.AddDynamic(this,&UMH_StartWidget::OnClickedAddPictureButton);
 	Btn_ForgotPassword->OnClicked.AddDynamic(this,&UMH_StartWidget::OnClickedForgotPasswordButton);
-	Btn_SignIn->OnClicked.AddDynamic(this,&UMH_StartWidget::OnClickedSignInButton);
+	Btn_GoToLobby_SignIn->OnClicked.AddDynamic(this,&UMH_StartWidget::GoToLobby);
 	Btn_SignUp->OnClicked.AddDynamic(this,&UMH_StartWidget::OnClickedSignUpButton);
 	Btn_FAN->OnClicked.AddDynamic(this,&UMH_StartWidget::OnClickedFANButton);
 	Btn_MANAGER->OnClicked.AddDynamic(this,&UMH_StartWidget::OnClickedMANAGERButton);
+	Btn_SelectAvatarL->OnClicked.AddDynamic(this,&UMH_StartWidget::OnClickedSelectAvatarLButton);
+	Btn_SelectAvatarR->OnClicked.AddDynamic(this,&UMH_StartWidget::OnClickedSelectAvatarRButton);
+	Btn_GenderMale->OnClicked.AddDynamic(this,&UMH_StartWidget::OnClickedGenderMaleButton);
+	Btn_GenderFeMale->OnClicked.AddDynamic(this,&UMH_StartWidget::OnClickedGenderFeMaleButton);
+	Btn_GoToLobby_Avatar->OnClicked.AddDynamic(this,&UMH_StartWidget::OnClickedAvatarConfirmButton);
+
 	
-	
+}
+
+void UMH_StartWidget::GoToLobby()
+{
+	//로비맵으로 이동(세션 생성,입장)
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Create Lobby Session"));
 }
 
 void UMH_StartWidget::OnClickedSignInButton()
 {
-	//메인맵으로 이동
+	//유저정보 확인
+	
+	//로비로 이동
+	GoToLobby();
 }
 
 void UMH_StartWidget::OnClickedSignUpButton()
 {
 	//회원가입 스위쳐 이동
+	WS_StartWidgetSwitcher->SetActiveWidgetIndex(1);
 }
 
 void UMH_StartWidget::OnClickedExitButton()
@@ -48,12 +64,15 @@ void UMH_StartWidget::OnClickedConfirmButton()
 	//이메일, 비번 중복확인
 	//사진확인
 	//모두 입력했는지 확인
+	
 	//캐릭터 설정으로 이동
+	WS_StartWidgetSwitcher->SetActiveWidgetIndex(2);
 }
 
 void UMH_StartWidget::OnClickedBackButton()
 {
 	//Start ui로 이동
+	WS_StartWidgetSwitcher->SetActiveWidgetIndex(0);
 }
 
 void UMH_StartWidget::OnClickedFANButton()
@@ -70,4 +89,41 @@ void UMH_StartWidget::OnClickedAddPictureButton()
 {
 	//사진 서버로 전달
 	//사진 정상적인지 확인
+}
+
+void UMH_StartWidget::OnClickedSelectAvatarRButton()
+{
+	// 아바타 이미지 오른쪽 이미지로
+}
+
+void UMH_StartWidget::OnClickedSelectAvatarLButton()
+{
+	//아바타 이미지 왼쪽이미지로
+}
+
+void UMH_StartWidget::OnClickedGenderMaleButton()
+{
+	//Gender == Male
+}
+
+void UMH_StartWidget::OnClickedGenderFeMaleButton()
+{
+	//Gender == FeMale
+}
+
+void UMH_StartWidget::OnClickedAvatarConfirmButton()
+{
+	//회원가입 완료-> 로비맵으로 이동(세션 생성,입장) - >콘서트 선택UI로 이동
+	
+	GoToLobby();
+}
+
+void UMH_StartWidget::OnClickedAvatarStyleAButton()
+{
+	//여자캐릭 or 남자캐릭?
+}
+
+void UMH_StartWidget::OnClickedAvatarStyleBButton()
+{
+	//여자캐릭 or 남자캐릭?
 }
