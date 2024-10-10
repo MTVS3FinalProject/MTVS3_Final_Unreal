b36a7d1695419d6fc57913403e2a8b623cda5ed6
may-04 <146904012+may-04@users.noreply.github.com>
2024-10-10 16:24:12 +0900

    MH : Start UI 작업중, Main UI 작업 시작 -> 미니맵, 시간 표시 먼저 시작할 예정

--- Source/MTVS3_Final/Public/JMH/MH_StartWidget.h
+++ Source/MTVS3_Final/Public/JMH/MH_StartWidget.h
@@ -15,94 +15,157 @@ class MTVS3_FINAL_API UMH_StartWidget : public UUserWidget
 	GENERATED_BODY()
 
 public:
-	void NativeConstruct() override;
+	
+	virtual void NativeConstruct() override;
 	
 	//Start
-	UPROPERTY(meta=(BindWidget))
+	UPROPERTY(VisibleAnywhere,meta=(BindWidget))
 	class UWidgetSwitcher* WS_StartWidgetSwitcher;
 	
 	//Login
-	UPROPERTY(meta=(BindWidget))
+	
+	UFUNCTION()
+	void GoToLobby();
+	
+	UPROPERTY(VisibleAnywhere,meta=(BindWidget))
 	class UCanvasPanel* Can_Login;
 
-	UPROPERTY(meta=(BindWidget))
-	class UButton*  Btn_SignIn;
+	UPROPERTY(VisibleAnywhere,meta=(BindWidget))
+	class UButton*  Btn_GoToLobby_SignIn;
 
 	UFUNCTION()
 	void OnClickedSignInButton();
 	
-	UPROPERTY(meta=(BindWidget))
+	UPROPERTY(VisibleAnywhere,meta=(BindWidget))
 	class UButton*  Btn_SignUp;
 
 	UFUNCTION()
 	void OnClickedSignUpButton();
 	
-	UPROPERTY(meta=(BindWidget))
+	UPROPERTY(VisibleAnywhere,meta=(BindWidget))
 	class UButton*  Btn_Exit;
 
 	UFUNCTION()
 	void OnClickedExitButton();
 	
-	UPROPERTY(meta=(BindWidget))
+	UPROPERTY(VisibleAnywhere,meta=(BindWidget))
 	class UButton*  Btn_ForgotPassword;
 
 	UFUNCTION()
 	void OnClickedForgotPasswordButton();
 
-	UPROPERTY(meta=(BindWidget))
+	UPROPERTY(VisibleAnywhere,meta=(BindWidget))
 	class UEditableText* EText_Email;
 	
-	UPROPERTY(meta=(BindWidget))
+	UPROPERTY(VisibleAnywhere,meta=(BindWidget))
 	class UEditableText* EText_PassWord;
 
 	//SignUp
 	
-	UPROPERTY(meta=(BindWidget))
+	UPROPERTY(VisibleAnywhere,meta=(BindWidget))
 	class UCanvasPanel* Can_SignUp;
 
-	UPROPERTY(meta=(BindWidget))
+	UPROPERTY(VisibleAnywhere,meta=(BindWidget))
 	class UButton*  Btn_Confirm;
 	
 	UFUNCTION()
 	void OnClickedConfirmButton();
 	
-	UPROPERTY(meta=(BindWidget))
+	UPROPERTY(VisibleAnywhere,meta=(BindWidget))
 	class UButton*  Btn_Back;
 	
 	UFUNCTION()
 	void OnClickedBackButton();
 
-	UPROPERTY(meta=(BindWidget))
+	UPROPERTY(VisibleAnywhere,meta=(BindWidget))
 	class UButton*  Btn_FAN;
 	
 	UFUNCTION()
 	void OnClickedFANButton();
 	
-	UPROPERTY(meta=(BindWidget))
+	UPROPERTY(VisibleAnywhere,meta=(BindWidget))
 	class UButton*  Btn_MANAGER;
 	
 	UFUNCTION()
 	void OnClickedMANAGERButton();
 	
-	UPROPERTY(meta=(BindWidget))
+	UPROPERTY(VisibleAnywhere,meta=(BindWidget))
 	class UComboBoxString*  Com_SetAge;
 	
-	UPROPERTY(meta=(BindWidget))
+	UPROPERTY(VisibleAnywhere,meta=(BindWidget))
 	class UButton*  Btn_AddPicture;
 
 	UFUNCTION()
 	void OnClickedAddPictureButton();
 	
-	UPROPERTY(meta=(BindWidget))
+	UPROPERTY(VisibleAnywhere,meta=(BindWidget))
 	class UEditableText* EText_SignupEmail;
 		
-	UPROPERTY(meta=(BindWidget))
+	UPROPERTY(VisibleAnywhere,meta=(BindWidget))
 	class UEditableText* EText_SignupPassWord;
 
 
 	//Select Avatar
 
-	UPROPERTY(meta=(BindWidget))
+	UPROPERTY(VisibleAnywhere,meta=(BindWidget))
 	class UCanvasPanel* Can_Avatar;
+
+	UPROPERTY(VisibleAnywhere,meta=(BindWidget))
+	class UImage* Img_Avatar;
+	
+	UPROPERTY(VisibleAnywhere,meta=(BindWidget))
+	class UImage* Img_Step1;
+
+	UPROPERTY(VisibleAnywhere,meta=(BindWidget))
+	class UImage* Img_Step2;
+
+	UPROPERTY(VisibleAnywhere,meta=(BindWidget))
+	class UImage* Img_Step3;
+	
+	UPROPERTY(VisibleAnywhere,meta=(BindWidget))
+	class UButton*  Btn_SelectAvatarR;
+
+	UFUNCTION()
+	void OnClickedSelectAvatarRButton();
+
+	UPROPERTY(VisibleAnywhere,meta=(BindWidget))
+	class UButton*  Btn_SelectAvatarL;
+	
+	UFUNCTION()
+	void OnClickedSelectAvatarLButton();
+
+	UPROPERTY(VisibleAnywhere,meta=(BindWidget))
+	class UEditableText* EText_Nickname;
+	
+	UPROPERTY(VisibleAnywhere,meta=(BindWidget))
+	class UButton*  Btn_GenderMale;
+		
+	UFUNCTION()
+	void OnClickedGenderMaleButton();
+		
+	UPROPERTY(VisibleAnywhere,meta=(BindWidget))
+	class UButton*  Btn_GenderFeMale;
+
+	UFUNCTION()
+	void OnClickedGenderFeMaleButton();
+			
+	UPROPERTY(VisibleAnywhere,meta=(BindWidget))
+	class UButton*  Btn_GoToLobby_Avatar;
+	
+	UFUNCTION()
+	void OnClickedAvatarConfirmButton();
+	
+	UPROPERTY(VisibleAnywhere,meta=(BindWidget))
+	class UButton*  Btn_AvatarStyleA;
+	
+	UFUNCTION()
+	void OnClickedAvatarStyleAButton();
+	
+	UPROPERTY(VisibleAnywhere,meta=(BindWidget))
+	class UButton*  Btn_AvatarStyleB;
+	
+	UFUNCTION()
+	void OnClickedAvatarStyleBButton();
+	
 	
 };
