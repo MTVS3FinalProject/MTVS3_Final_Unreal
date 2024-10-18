542504955b6f26803d1750be23e9ad44e5f5d6de
g0r0kke <jiniandjuni@gmail.com>
2024-10-18 19:08:39 +0900

    Fix: 플레이어 컨트롤러에 MainUI 참조 전달

--- Source/MTVS3_Final/Private/HJ/TTPlayer.cpp
+++ Source/MTVS3_Final/Private/HJ/TTPlayer.cpp
@@ -18,6 +18,7 @@
 #include "Engine/World.h"
 #include "JMH/MH_TicketingWidget.h"
 #include "JMH/MainWidget.h"
+#include "LHM/TTPlayerController.h"
 
 // Sets default values
 ATTPlayer::ATTPlayer()
@@ -300,10 +301,16 @@ void ATTPlayer::OnMyActionChat(const FInputActionValue& Value)
 
 void ATTPlayer::InitMainUI()
 {
-	MainUI = CastChecked<UMainWidget>(CreateWidget(GetWorld() , MainUIFactory));
+	MainUI = Cast<UMainWidget>(CreateWidget(GetWorld() , MainUIFactory));
 	if ( MainUI )
 	{
 		MainUI->AddToViewport();
+	}
+
+	ATTPlayerController* MyController = Cast<ATTPlayerController>(GetController());
+	if ( MyController )
+	{
+		MyController->SetMainUI(MainUI);
 	}
 
 	TicketingUI = CastChecked<UMH_TicketingWidget>(CreateWidget(GetWorld() , TicketingUIFactory));
