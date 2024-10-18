b00079a3122815f830efa54d833f89998325a4f9
g0r0kke <jiniandjuni@gmail.com>
2024-10-18 17:01:16 +0900

    Feat: BP_LuckyDrawChair

---
+++ Source/MTVS3_Final/Public/HJ/LuckyDrawChair.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "LuckyDrawChair.generated.h"
+
+UCLASS()
+class MTVS3_FINAL_API ALuckyDrawChair : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ALuckyDrawChair();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditDefaultsOnly)
+	class UBoxComponent* BoxComp;
+
+	UPROPERTY(EditDefaultsOnly)
+	class UStaticMeshComponent* MeshComp;
+};
