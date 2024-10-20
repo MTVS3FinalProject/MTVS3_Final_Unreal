93e32e2bf775a9fdfdf9ccc98de1241f80b56069
may-04 <146904012+may-04@users.noreply.github.com>
2024-10-21 08:36:59 +0900

    [Feat] MH : Build.cs 파일에 "Paper2D" 추가

--- Source/MTVS3_Final/MTVS3_Final.Build.cs
+++ Source/MTVS3_Final/MTVS3_Final.Build.cs
@@ -9,7 +9,7 @@ public class MTVS3_Final : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG" , "HTTP", "Json" , "JsonUtilities", "OnlineSubsystem", "OnlineSubsystemSteam" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG" , "HTTP", "Json" , "JsonUtilities", "OnlineSubsystem", "OnlineSubsystemSteam" ,"Paper2D"});
 
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
