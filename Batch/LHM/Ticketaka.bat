972c2d6643cecc5bef44d739301db4b9343ebdbe
lee <hyeonmin1012@gmail.com>
2024-10-23 11:20:43 +0900

    [Feat] HM_HttpActor2 UserPhoneNumber int->string 변환 / 추첨 시작 알림 요청,응답 추가

--- Batch/LHM/Ticketaka.bat
+++
@@ -1,1 +0,0 @@
-"C:\Program Files\Epic Games\UE_5.4\Engine\Binaries\Win64\UnrealEditor.exe" C:\Projects\Unreal\MTVS3_Final_Unreal\MTVS3_Final.uproject" -game -ResX=640 -ResY=480 -WinX=200 -WinY=300 -log
