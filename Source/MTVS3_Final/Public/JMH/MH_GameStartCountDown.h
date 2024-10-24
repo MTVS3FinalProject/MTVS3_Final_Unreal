cc6633db9ebf57ab0526758a3860c9103b3db2df
may-04 <146904012+may-04@users.noreply.github.com>
2024-10-25 00:42:36 +0900

    MH : 룰렛위젯, 카운트다운 추가 .

---
+++ Source/MTVS3_Final/Public/JMH/MH_GameStartCountDown.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MH_GameStartCountDown.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MTVS3_FINAL_API UMH_GameStartCountDown : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	
+	virtual void NativeConstruct() override;
+
+	UPROPERTY()
+	int32 CountdownValue;
+	//카운트다운
+	UPROPERTY(meta = (BindWidget))
+	class UTextBlock* CountdownText;
+	UFUNCTION()
+	void StartCountdown();
+	UFUNCTION()
+	void UpdateCountdown();
+
+	UPROPERTY()
+	FTimerHandle CountdownTimerHandle;
+};
