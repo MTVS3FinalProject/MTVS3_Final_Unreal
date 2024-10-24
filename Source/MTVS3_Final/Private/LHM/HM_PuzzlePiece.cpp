b2e48f0edd87cc69fca4ebf45c49dab60f3269d1
lee <hyeonmin1012@gmail.com>
2024-10-25 01:04:09 +0900

    [Feat] 퍼즐 플레이어, 피스액터, 트리거볼륨 생성

---
+++ Source/MTVS3_Final/Private/LHM/HM_PuzzlePiece.cpp
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LHM/HM_PuzzlePiece.h"
+
+// Sets default values
+AHM_PuzzlePiece::AHM_PuzzlePiece()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+    Piece = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Piece"));
+
+    RootComponent = Piece;
+
+    static ConstructorHelpers::FObjectFinder<UStaticMesh> MeshAsset(TEXT("/Script/Engine.StaticMesh'/Engine/BasicShapes/Cube.Cube'"));
+    if ( MeshAsset.Succeeded() )
+    {
+        Piece->SetStaticMesh(MeshAsset.Object);
+    }
+
+    Piece->SetSimulatePhysics(true);
+    Piece->SetCollisionProfileName(UCollisionProfile::BlockAll_ProfileName);
+    Piece->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+}
+
+// Called when the game starts or when spawned
+void AHM_PuzzlePiece::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AHM_PuzzlePiece::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/MTVS3_Final/Public/LHM/HM_PuzzlePlayer.h
@@ -0,0 +1,96 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "../../../../Plugins/EnhancedInput/Source/EnhancedInput/Public/InputActionValue.h"
+#include "HM_PuzzlePlayer.generated.h"
+
+UCLASS()
+class MTVS3_FINAL_API AHM_PuzzlePlayer : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AHM_PuzzlePlayer();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(EditDefaultsOnly)
+	class USpringArmComponent* SpringArmComp;
+
+	UPROPERTY(EditDefaultsOnly)
+	class UCameraComponent* TPSCameraComp;
+
+	UPROPERTY(EditDefaultsOnly)
+	class UCameraComponent* FPSCameraComp;
+
+	UPROPERTY(EditInstanceOnly , Category = "TTSettings|State")
+	bool bIsThirdPerson = true;
+	void SwitchCamera(bool _bIsThirdPerson);
+
+	UPROPERTY(EditAnywhere , Category = "TTSettings|Custom")
+	float WalkSpeed = 500.0f;
+	UPROPERTY(EditAnywhere , Category = "TTSettings|Custom")
+	float RunSpeed = 800.0f;
+
+#pragma region 입력
+	UPROPERTY(EditDefaultsOnly , Category = "TTSettings|Input")
+	class UInputMappingContext* IMC_PuzzlePlayer;
+
+	UPROPERTY(EditDefaultsOnly , Category = "TTSettings|Input")
+	class UInputAction* IA_Move;
+	void OnMyActionMove(const FInputActionValue& Value);
+
+	FVector Direction;
+	
+	UPROPERTY(EditDefaultsOnly , Category = "TTSettings|Input")
+	class UInputAction* IA_EnableLook;
+	void OnMyActionEnableLookStart(const FInputActionValue& Value);
+	void OnMyActionEnableLookComplete(const FInputActionValue& Value);
+	bool bIsEnableLook = false;
+
+	UPROPERTY(EditDefaultsOnly , Category = "TTSettings|Input")
+	class UInputAction* IA_Look;
+	void OnMyActionLook(const FInputActionValue& Value);
+
+	UPROPERTY(EditDefaultsOnly , Category = "TTSettings|Input")
+	class UInputAction* IA_Jump;
+	void OnMyActionJumpStart(const FInputActionValue& Value);
+	void OnMyActionJumpComplete(const FInputActionValue& Value);
+
+	UPROPERTY(EditDefaultsOnly , Category = "TTSettings|Input")
+	class UInputAction* IA_Run;
+	void OnMyActionRunStart(const FInputActionValue& Value);
+	void OnMyActionRunComplete(const FInputActionValue& Value);
+
+	UPROPERTY(EditDefaultsOnly , Category = "TTSettings|Input")
+	class UInputAction* IA_Pickup;
+	UPROPERTY(EditDefaultsOnly , Category = "TTSettings|Input")
+	class UInputAction* IA_Launch;
+	void OnMyActionPickupPiece(const FInputActionValue& Value);
+	void OnMyActionLaunchPickedUpPiece(const FInputActionValue& Value);
+#pragma endregion
+
+	bool bIsHoldingPiece;
+
+	void PickupPiece();
+
+	void LaunchPickedUpPiece();
+
+	bool HasPickedUpPiece() const { return PickedUpPiece != nullptr; }
+
+private:
+	class AHM_PuzzlePiece* PickedUpPiece;
+};
