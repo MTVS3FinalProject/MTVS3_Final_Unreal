255f188825857239d9b70b37f1016c9f89802e62
may-04 <146904012+may-04@users.noreply.github.com>
2024-10-24 13:53:32 +0900

    MH : 집가기전 커밋

--- Source/MTVS3_Final/Private/JMH/MH_Minmimap.cpp
+++ Source/MTVS3_Final/Private/JMH/MH_Minmimap.cpp
@@ -95,33 +95,35 @@ void AMH_Minmimap::Tick(float DeltaTime)
 void AMH_Minmimap::ApplyMinimap()
 {
 	// 오너 캐릭터 확인 및 미니맵 업데이트
-	Player = UGameplayStatics::GetPlayerCharacter(this, 0); 
-	if (Player)
-	{
-		//카메라 위치 업뎃
-		FVector PlayerLocation = Player->GetActorLocation();
-		FVector MinimapCameraLocation = FVector(PlayerLocation.X , PlayerLocation.Y , PlayerLocation.Z + 500.f);
-		MinimapCameraBoom->SetWorldLocation(MinimapCameraLocation);
-
-		MinimapSprite->SetWorldLocation(FVector(PlayerLocation.X , PlayerLocation.Y , PlayerLocation.Z + 300.f));
-
-		//플레이어 회전값
-		FRotator PlayerRotation = Player->GetActorRotation();
-
-		// 플레이어의 카메라 회전값 가져오기
-		FRotator CameraRotation;
-		if (Player->GetController())
+	Player = UGameplayStatics::GetPlayerCharacter(this, 0);
+	//APlayerController* PlayerController = Cast<APlayerController>(GetOwner());
+	
+		if (Player)
 		{
-			CameraRotation = Player->GetControlRotation();  // 플레이어의 카메라(컨트롤러) 회전 값
-		}
-		// 미니맵 캡처의 회전을 카메라의 회전 값에 맞춰 설정
-		FRotator MinimapRotation = FRotator(-90.f, CameraRotation.Yaw, 0.f);
-		MinimapCapture->SetWorldRotation(MinimapRotation);
+			//카메라 위치 업뎃
+			FVector PlayerLocation = Player->GetActorLocation();
+			FVector MinimapCameraLocation = FVector(PlayerLocation.X , PlayerLocation.Y , PlayerLocation.Z + 500.f);
+			MinimapCameraBoom->SetWorldLocation(MinimapCameraLocation);
+
+			MinimapSprite->SetWorldLocation(FVector(PlayerLocation.X , PlayerLocation.Y , PlayerLocation.Z + 300.f));
+
+			//플레이어 회전값
+			FRotator PlayerRotation = Player->GetActorRotation();
+
+			// 플레이어의 카메라 회전값 가져오기
+			FRotator CameraRotation;
+			if (Player->GetController())
+			{
+				CameraRotation = Player->GetControlRotation();  // 플레이어의 카메라(컨트롤러) 회전 값
+			}
+			// 미니맵 캡처의 회전을 카메라의 회전 값에 맞춰 설정
+			FRotator MinimapRotation = FRotator(-90.f, CameraRotation.Yaw, 0.f);
+			MinimapCapture->SetWorldRotation(MinimapRotation);
 		
-		//플레이어의 회전 포인트 이미지에 적용
-		FRotator MinimapSpriteRotation = FRotator(FRotator::MakeFromEuler(FVector(90.f , 0.f , PlayerRotation.Yaw-90.f)));
-		MinimapSprite->SetWorldRotation(MinimapSpriteRotation);
-	}
+			//플레이어의 회전 포인트 이미지에 적용
+			FRotator MinimapSpriteRotation = FRotator(FRotator::MakeFromEuler(FVector(90.f , 0.f , PlayerRotation.Yaw-90.f)));
+			MinimapSprite->SetWorldRotation(MinimapSpriteRotation);
+		}
 	
 }
 
