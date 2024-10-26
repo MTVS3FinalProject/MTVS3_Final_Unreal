aca9565332ccfb2464fbd91c26a1da07c86a5ac7
lee <hyeonmin1012@gmail.com>
2024-10-27 04:43:04 +0900

    [Feat] PuzzlePlayer 줍기 놓기 동기화 / PuzzlePiece 위치 동기화중(ing...)

--- Source/MTVS3_Final/Private/LHM/HM_PuzzlePiece.cpp
+++ Source/MTVS3_Final/Private/LHM/HM_PuzzlePiece.cpp
@@ -3,6 +3,9 @@
 
 #include "LHM/HM_PuzzlePiece.h"
 
+#include "Kismet/KismetMathLibrary.h"
+#include "Net/UnrealNetwork.h"
+
 // Sets default values
 AHM_PuzzlePiece::AHM_PuzzlePiece()
 {
@@ -11,7 +14,7 @@ AHM_PuzzlePiece::AHM_PuzzlePiece()
 
     Piece = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Piece"));
 
-    Piece->SetupAttachment(RootComponent);
+    //Piece->SetupAttachment(RootComponent);
 
     static ConstructorHelpers::FObjectFinder<UStaticMesh> MeshAsset(TEXT("/Script/Engine.StaticMesh'/Engine/BasicShapes/Cube.Cube'"));
     if ( MeshAsset.Succeeded() )
@@ -30,6 +33,7 @@ void AHM_PuzzlePiece::BeginPlay()
 	Super::BeginPlay();
 	
 	bReplicates = true;
+
 }
 
 // Called every frame
@@ -38,4 +42,44 @@ void AHM_PuzzlePiece::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 }
+
+void AHM_PuzzlePiece::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(AHM_PuzzlePiece, CurrentTransform);
+	DOREPLIFETIME(AHM_PuzzlePiece, bSimulatingPhysics);
+}
+
+void AHM_PuzzlePiece::OnRep_PieceTransform()
+{
+	if (GetPiece() && !bSimulatingPhysics)
+	{
+		GetPiece()->SetWorldTransform(CurrentTransform);
+	}
+}
+
+// void AHM_PuzzlePiece::Server_SetPieceLocation_Implementation(const FVector& NewLocation)
+// {
+// 	// 새로운 위치를 설정하고 동기화
+// 	CurrentTransform.SetLocation(NewLocation);
+// 	OnRep_PieceTransform(); // 동기화 함수 호출
+// }
+//
+// // 퍼즐 조각을 놓을 때 이 함수를 호출
+// void AHM_PuzzlePiece::PlacePiece(const FVector& NewLocation)
+// {
+// 	if (HasAuthority()) // 서버에서 직접 호출된 경우
+// 	{
+// 		CurrentTransform.SetLocation(NewLocation);
+// 		OnRep_PieceTransform();
+// 	}
+// 	else // 클라이언트에서 호출된 경우
+// 	{
+// 		Server_SetPieceLocation(NewLocation); // 서버에 요청
+// 	}
+// }
+
+
+
 
--- Source/MTVS3_Final/Public/LHM/HM_PuzzlePiece.h
+++ Source/MTVS3_Final/Public/LHM/HM_PuzzlePiece.h
@@ -29,4 +29,26 @@ public:
 	// 스태틱 메시 컴포넌트 반환 함수 추가
 	UStaticMeshComponent* GetPiece() const { return Piece; }
 
+	// --------------- Multiplayer 요소들 ---------------
+	
+	// 물리 시뮬레이션 상태를 복제하기 위한 함수들
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+    
+	// 현재 Transform을 서버에서 클라이언트로 복제
+	UPROPERTY(ReplicatedUsing = OnRep_PieceTransform)
+	FTransform CurrentTransform;
+
+	UFUNCTION()
+	void OnRep_PieceTransform();
+
+	// 물리 시뮬레이션 상태
+	UPROPERTY(Replicated)
+	bool bSimulatingPhysics;
+
+	// // 클라이언트가 조각을 놓을 때 호출할 RPC 함수
+	// UFUNCTION(Server, Reliable)
+	// void Server_SetPieceLocation(const FVector& NewLocation);
+	//
+	// void PlacePiece(const FVector& NewLocation);
+	// //void Server_SetPieceLocation_Implementation(const FVector& NewLocation);
 };
