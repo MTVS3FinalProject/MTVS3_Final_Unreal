d2e6d9a89487c5994dcde5847041b47fe6fb36fe
may-04 <146904012+may-04@users.noreply.github.com>
2024-11-06 13:51:53 +0900

    [Feat]MH: 인벤토리 UI 작업중

---
+++ Source/MTVS3_Final/Private/JMH/MH_Inventory.cpp
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "JMH/MH_Inventory.h"
+
+#include "Components/WidgetSwitcher.h"
+
+void UMH_Inventory::NativeConstruct()
+{
+	Super::NativeConstruct();
+	//칭호에서 시작
+	OnClicked_PlayerTitle();
+}
+
+void UMH_Inventory::SetWidgetSwitcher(int32 num)
+{
+	WS_InvenWidgetSwitcher->SetActiveWidgetIndex(num);
+}
+
+void UMH_Inventory::OnClickedBack_Inven()
+{
+	//뒤로가기, mainUI 인벤꺼지게 델리게이트.
+}
+
+void UMH_Inventory::OnClicked_PlayerTitle()
+{
+	//칭호
+	SetWidgetSwitcher(0);
+	//현민
+	//여기서 칭호 정보 받아오기
+	//매희 - 셋해주는 함수 만들기
+}
+
+void UMH_Inventory::OnClicked_Ticket()
+{
+	//티켓
+	SetWidgetSwitcher(1);
+	//현민
+	//여기서 티켓 정보 받아오기
+	//매희 - 셋해주는 함수 만들기
+}
+
+void UMH_Inventory::OnClicked_Sticker()
+{
+	//스티커
+	SetWidgetSwitcher(2);
+	//현민
+	//여기서 스티커 정보 받아오기
+	//매희 - 셋해주는 함수 만들기
+}
+
---
+++ Source/MTVS3_Final/Public/JMH/MH_Inventory.h
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MH_Inventory.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MTVS3_FINAL_API UMH_Inventory : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+
+	virtual void NativeConstruct() override;
+
+	
+	UPROPERTY(VisibleAnywhere,meta=(BindWidget))
+	class UWidgetSwitcher* WS_InvenWidgetSwitcher;
+	//위젯스위처설정 
+	UFUNCTION()
+	void SetWidgetSwitcher(int32 num);//0:칭호,1:티켓,2:스티커
+	
+	UPROPERTY(VisibleAnywhere,meta=(BindWidget))
+	class UButton* Btn_Back_Inven;
+	UFUNCTION()
+	void OnClickedBack_Inven();
+
+	UPROPERTY(VisibleAnywhere,meta=(BindWidget))
+	class UHorizontalBox* Hori_InvenBox_00_PlayerTitle;
+
+	UPROPERTY(VisibleAnywhere,meta=(BindWidget))
+	class UHorizontalBox* Hori_InvenBox_01_Ticket;
+	
+	UPROPERTY(VisibleAnywhere,meta=(BindWidget))
+	class UHorizontalBox* Hori_InvenBox_02_Sticker;
+	
+	UPROPERTY(VisibleAnywhere,meta=(BindWidget))
+	class UButton* Btn_00_PlayerTitle;
+	UFUNCTION()
+	void OnClicked_PlayerTitle();
+	
+	UPROPERTY(VisibleAnywhere,meta=(BindWidget))
+	class UButton* Btn_01_Ticket;
+	UFUNCTION()
+	void OnClicked_Ticket();
+
+	UPROPERTY(VisibleAnywhere,meta=(BindWidget))
+	class UButton* Btn_02_Sticker;
+	UFUNCTION()
+	void OnClicked_Sticker();
+};
