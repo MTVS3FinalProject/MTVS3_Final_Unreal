9f7c469828cdfc88b64bde81453555d0225208a2
g0r0kke <jiniandjuni@gmail.com>
2024-11-08 16:21:59 +0900

    Feat: 추첨 초청 시 1분 30초 후 시작되게 UI 텍스트 설정, TA 추첨맵에서 테이블 뺌, 티켓팅위젯 접수 취소 바인딩 수정
    플레이어 컨트롤러 멀티캐스트 RPC 불가능하고, 플레이어에서 멀티캐스트RPC 해봤는데 UI 반영이 안 돼서 플레이어컨트롤러 각자 자기 추첨 시작 시간 설정하게 함 /
    그래서 Hall에서 추첨 초대할 때 호스트에게 추첨 시작 시간 알리고, 호스트가 아닐 경우 클라이언트 RPC 보내는 방식

--- Source/MTVS3_Final/Private/HJ/TTHallGameState.cpp
+++ Source/MTVS3_Final/Private/HJ/TTHallGameState.cpp
@@ -8,18 +8,19 @@
 #include "HJ/TTPlayerState.h"
 #include "JMH/MainWidget.h"
 #include "JMH/MH_TicketingWidget.h"
+#include "LHM/TTPlayerController.h"
 
 void ATTHallGameState::BeginPlay()
 {
 	Super::BeginPlay();
 
 	UTTGameInstance* GI = GetWorld()->GetGameInstance<UTTGameInstance>();
-	if ( GI ) GI->SetPlaceState(EPlaceState::Plaza);
+	if (GI) GI->SetPlaceState(EPlaceState::Plaza);
 
-	for ( TActorIterator<ATTPlayer> It(GetWorld()); It; ++It )
+	for (TActorIterator<ATTPlayer> It(GetWorld()); It; ++It)
 	{
 		ATTPlayer* TTPlayer = *It;
-		if ( TTPlayer && TTPlayer->IsLocallyControlled() )
+		if (TTPlayer && TTPlayer->IsLocallyControlled())
 		{
 			TTPlayer->InitMainUI();
 			TTPlayer->ServerSetNickname(GI->GetNickname());
@@ -28,62 +29,76 @@ void ATTHallGameState::BeginPlay()
 	}
 }
 
-void ATTHallGameState::SendLuckyDrawInvitation(const TArray<FString>& NicknameList, int32 CompetitionRate)
+void ATTHallGameState::SendLuckyDrawInvitation(const TArray<FString>& NicknameList , int32 CompetitionRate)
 {
 	if (!GetWorld() || NicknameList.Num() == 0)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("GetWorld() is invalid or NicknameList is empty."));
+		UE_LOG(LogTemp , Warning , TEXT("GetWorld() is invalid or NicknameList is empty."));
 		return;
 	}
-	
-	for ( TActorIterator<ATTPlayer> It(GetWorld()); It; ++It )
+
+	for (TActorIterator<ATTPlayer> It(GetWorld()); It; ++It)
 	{
 		ATTPlayer* TTPlayer = *It;
-		if ( TTPlayer && !TTPlayer->bIsHost)
+		if (TTPlayer && TTPlayer->bIsHost)
 		{
-			if (NicknameList.Contains(TTPlayer->GetNickname()))
+			if (TTPlayer->bIsHost) // 호스트에게는 추첨 시작 시간을 알림
 			{
-				TTPlayer->ClientShowLuckyDrawInvitation(true, CompetitionRate);
+				ATTPlayerController* TTPC = Cast<ATTPlayerController>(TTPlayer->GetController());
+				if (TTPC)
+				{
+					TTPC->SetDrawStartTime();
+				}
+			}
+			else // 호스트가 아닐 때는 클라이언트 RPC로 초청
+			{
+				if (TTPlayer && !TTPlayer->bIsHost)
+				{
+					if (NicknameList.Contains(TTPlayer->GetNickname()))
+					{
+						TTPlayer->ClientShowLuckyDrawInvitation(true , CompetitionRate);
+					}
+				}
 			}
 		}
 	}
 
 	FTimerHandle HideLDInvitationTimerHandle;
-	GetWorldTimerManager().SetTimer(HideLDInvitationTimerHandle, [this, NicknameList, CompetitionRate]()
+	GetWorldTimerManager().SetTimer(HideLDInvitationTimerHandle , [this, NicknameList, CompetitionRate]()
 	{
-		HideLuckyDrawInvitation(NicknameList, CompetitionRate);
-	}, 90.0f, false);
+		HideLuckyDrawInvitation(NicknameList , CompetitionRate);
+	} , 90.0f , false);
 }
 
-void ATTHallGameState::HideLuckyDrawInvitation(const TArray<FString>& NicknameList, int32 CompetitionRate)
+void ATTHallGameState::HideLuckyDrawInvitation(const TArray<FString>& NicknameList , int32 CompetitionRate)
 {
 	if (!GetWorld() || NicknameList.Num() == 0)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("GetWorld() is invalid or NicknameList is empty."));
+		UE_LOG(LogTemp , Warning , TEXT("GetWorld() is invalid or NicknameList is empty."));
 		return;
 	}
-	
-	for ( TActorIterator<ATTPlayer> It(GetWorld()); It; ++It )
+
+	for (TActorIterator<ATTPlayer> It(GetWorld()); It; ++It)
 	{
 		ATTPlayer* TTPlayer = *It;
 
 		if (!TTPlayer)
 		{
-			UE_LOG(LogTemp, Warning, TEXT("Found null TTPlayer"));
+			UE_LOG(LogTemp , Warning , TEXT("Found null TTPlayer"));
 			continue;
 		}
 
 		if (!IsValid(TTPlayer))
 		{
-			UE_LOG(LogTemp, Warning, TEXT("Found invalid TTPlayer"));
+			UE_LOG(LogTemp , Warning , TEXT("Found invalid TTPlayer"));
 			continue;
 		}
-		
-		if ( !TTPlayer->bIsHost)
+
+		if (!TTPlayer->bIsHost)
 		{
 			if (NicknameList.Contains(TTPlayer->GetNickname()))
 			{
-				TTPlayer->ClientShowLuckyDrawInvitation(false, CompetitionRate);
+				TTPlayer->ClientShowLuckyDrawInvitation(false , CompetitionRate);
 			}
 		}
 	}
