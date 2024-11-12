5866a8e557ce6b6c65d1e47a82c1e15673415ad6
g0r0kke <jiniandjuni@gmail.com>
2024-11-12 14:03:44 +0900

    Feat: 상호작용 가능한 의자 아웃라인 표시, 플레이어 아바타 UPROPERTY로 변경, 플레이어 및 의자 콜리전 변경
    앉기 애니메이션 멀티캐스트 RPC 버그 수정 진행 중

--- Source/MTVS3_Final/Public/HJ/TTPlayer.h
+++ Source/MTVS3_Final/Public/HJ/TTPlayer.h
@@ -193,7 +193,7 @@ public:
 #pragma endregion
 
 #pragma region 플레이어 정보 및 복제 설정
-	void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const;
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const;
 
 	UPROPERTY(ReplicatedUsing=OnRep_Nickname , VisibleAnywhere , Category = "TTSettings|UserInfo")
 	FString Nickname;
@@ -281,6 +281,7 @@ public:
 	UPROPERTY(Transient)
 	class ULevelSequencePlayer* SequencePlayer;
 
+	UFUNCTION(BlueprintCallable , Category = "TTSettings|Avatar")
 	void SetNewSkeletalMesh(const int32& _AvatarData);
 
 	// ====================퍼즐====================
@@ -402,13 +403,13 @@ public:
 	UPROPERTY()
 	class UMH_WorldMap* WorldMapUI;
 
-	UPROPERTY(EditAnywhere , Category = "TTSettings|UI")
+	UPROPERTY(EditAnywhere ,BlueprintReadWrite, Category = "TTSettings|UI")
 	class UWidgetComponent* NicknameUIComp;
 	UPROPERTY(EditAnywhere , Category = "TTSettings|UI")
 	TSubclassOf<class UPlayerNicknameWidget> NicknameUIFactory;
 	class UPlayerNicknameWidget* NicknameUI;
 
-	UPROPERTY(EditAnywhere , Category = "TTSettings|UI")
+	UPROPERTY(EditAnywhere ,BlueprintReadWrite, Category = "TTSettings|UI")
 	class UWidgetComponent* TitleUIComp;
 	UPROPERTY(EditAnywhere , Category = "TTSettings|UI")
 	TSubclassOf<class UPlayerTitleWidget> TitleUIFactory;
@@ -457,7 +458,7 @@ public:
 	UFUNCTION()
 	void CreateMinimapActor();
 	
-	UPROPERTY(EditAnywhere , Category = "TTSettings|UI")
+	UPROPERTY(EditAnywhere , BlueprintReadWrite, Category = "TTSettings|UI")
 	class UWidgetComponent*  EmojiComp;
 	
 	UPROPERTY(EditAnywhere , Category = "TTSettings|UI")
@@ -465,4 +466,19 @@ public:
 
 	UPROPERTY(EditAnywhere,BlueprintReadWrite)
 	class UMH_EmojiImg* EmojiWidget;
+
+	UPROPERTY(EditAnywhere , Category = "TTSettings|Avatar")
+	USkeletalMesh* Avatar1Mesh;
+
+	UPROPERTY(EditAnywhere , Category = "TTSettings|Avatar")
+	USkeletalMesh* Avatar2Mesh;
+
+	UPROPERTY(EditAnywhere , Category = "TTSettings|Avatar")
+	USkeletalMesh* Avatar3Mesh;
+
+	UPROPERTY(EditAnywhere , Category = "TTSettings|Avatar")
+	USkeletalMesh* Avatar4Mesh;
+
+	UPROPERTY(EditAnywhere , Category = "TTSettings|Avatar")
+	USkeletalMesh* ManagerMesh;
 };
