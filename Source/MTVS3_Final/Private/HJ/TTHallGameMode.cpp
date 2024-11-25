3f9024e411f2cbd123ccb65f941f1aa922721937
g0r0kke <jiniandjuni@gmail.com>
2024-11-25 12:58:17 +0900

    Feat: 게임모드 스크린디버그 끄기 변수

--- Source/MTVS3_Final/Private/HJ/TTHallGameMode.cpp
+++ Source/MTVS3_Final/Private/HJ/TTHallGameMode.cpp
@@ -8,6 +8,14 @@
 void ATTHallGameMode::BeginPlay()
 {
 	Super::BeginPlay();
+
+	if (!bEnableScreenDebug)
+	{
+#if UE_BUILD_DEVELOPMENT
+		GEngine->bEnableOnScreenDebugMessages = false; // 화면 디버그 메시지 비활성화
+		LogTemp.SetVerbosity(ELogVerbosity::NoLogging); // LogTemp 카테고리 로깅 비활성화
+#endif
+	}
     
 	// 모든 PlayerStart를 찾아서 캐시
 	UGameplayStatics::GetAllActorsOfClass(GetWorld(), APlayerStart::StaticClass(), CachedPlayerStarts);
