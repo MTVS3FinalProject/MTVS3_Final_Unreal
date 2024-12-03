abc8378d6f6c5507f9d17aeec36f723263b3803c
may-04 <146904012+may-04@users.noreply.github.com>
2024-12-04 01:21:36 +0900

    MH : 로그인 에러메세지 확인완료 HttpActor에 446줄 StartUI->ShowLoginErrorMessage(*ErrorMessage); 추가 해야함

--- Source/MTVS3_Final/Public/JMH/MH_ErrorMessage.h
+++ Source/MTVS3_Final/Public/JMH/MH_ErrorMessage.h
@@ -9,17 +9,34 @@
 /**
  * 
  */
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnClickedErrorExit);
+
 UCLASS()
 class MTVS3_FINAL_API UMH_ErrorMessage : public UUserWidget
 {
 	GENERATED_BODY()
 
 	virtual void NativeConstruct() override;
+public:
+	FOnClickedErrorExit OnClickedErrorExit;
 
+	UFUNCTION()
+	void CloseBtnPressedErrorExit()
+	{
+		// 델리게이트 호출
+		OnClickedErrorExit.Broadcast();
+	}
+	
 	UPROPERTY(meta=(BindWidget))
 	class UTextBlock* Text_ErrorMessage;
+
+	UFUNCTION()
+	void SetTextMessage(FString ErrorMS);
 	
 	UPROPERTY(meta=(BindWidget))
 	class UButton* Btn_ErrorExit;
+	
+	UFUNCTION()
+	void ShowErrorMessage(FString ErrorMS);
 	
 };
