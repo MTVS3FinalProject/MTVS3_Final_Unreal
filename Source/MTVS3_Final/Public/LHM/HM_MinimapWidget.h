6bc30bb1d79263ce341e9f38ebdaf63c5fad3479
lee <hyeonmin1012@gmail.com>
2024-12-14 11:07:26 +0900

    [Feat] MinimapWidget 키오스크, 스타일라운지, 커뮤니티홀 Switch State Visible

--- Source/MTVS3_Final/Public/LHM/HM_MinimapWidget.h
+++ Source/MTVS3_Final/Public/LHM/HM_MinimapWidget.h
@@ -25,6 +25,29 @@ public:
 	class UImage* Img_StyleLounge;
 	FVector StyleLounge = FVector(18000.0f, 4900.0f, 3300.0f);
 	
+	UPROPERTY(VisibleAnywhere , meta=(BindWidget))
+	class UImage* Img_Community;
+	FVector Community = FVector(22220.0f, 1990.0f, 3300.0f);
+	
+	UPROPERTY(VisibleAnywhere , meta=(BindWidget))
+	class UImage* Img_Kiosk_1;
+	FVector Kiosk_1 = FVector(17318.0f, 1342.0f, 3300.0f);
+	
+	UPROPERTY(VisibleAnywhere , meta=(BindWidget))
+	class UImage* Img_Kiosk_2;
+	FVector Kiosk_2 = FVector(18652.0f, 1341.0f, 3300.0f);
+	
+	UPROPERTY(VisibleAnywhere , meta=(BindWidget))
+	class UImage* Img_Kiosk_3;
+	FVector Kiosk_3 = FVector(18657.0f, 2661.0f, 3300.0f);
+	
+	UPROPERTY(VisibleAnywhere , meta=(BindWidget))
+	class UImage* Img_Kiosk_4;
+	FVector Kiosk_4 = FVector(17358.0f, 2666.0f, 3300.0f);
+
+	UFUNCTION()
+	void SetImgsVisible(bool visible);
+	
 	UFUNCTION()
 	void UpdateTargetPosition(FVector TargetLocation, UImage* Img_Target);
 };
